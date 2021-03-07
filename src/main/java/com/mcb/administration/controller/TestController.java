f527f91f4165a8c689518a00310e131056198592
santhoshvfsd <santhoshvfsd@outlook.com>
2021-03-08 05:03:26 +0530

    basic swagger configuration with basic route

---
+++ src/main/java/com/mcb/administration/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.mcb.administration.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+    @GetMapping
+    public String testRoute () {
+        return  "test";
+    }
+
+}
