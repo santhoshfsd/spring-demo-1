5b8d4e3046fc8c207e4a428b9e22da68f512b16b
santhoshvfsd <santhoshvfsd@outlook.com>
2021-03-08 13:22:23 +0530

    api security

--- src/main/java/com/mcb/administration/controller/TestController.java
+++ src/main/java/com/mcb/administration/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.mcb.administration.controller;
 
+import com.mcb.administration.model.HttpResponse;
+import com.mcb.administration.model.JwtResponse;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/test")
 public class TestController {
 
+    @ApiOperation(
+            value = "List all people",
+            notes = "List all people using paging",
+            response = HttpResponse.class,
+            responseContainer = "List"
+    )
     @GetMapping
-    public String testRoute () {
-        return  "test";
+    public ResponseEntity<HttpResponse> testRoute () {
+        return  new ResponseEntity<HttpResponse>(new HttpResponse("test route success","",200),
+                HttpStatus.ACCEPTED);
     }
 
 }
